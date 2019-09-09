1a7d082ca88a58d258417203d0f8d6bd6eef705c
bezalel001 <mickeybezalel@gmail.com>
2019-09-09 12:16:48 +0100

    Added checkout page, cjeckout item component  and updated cart to use selectors from reselect

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,12 +4,21 @@ import { createSelector } from 'reselect';
 // input selector: selectors that returns a piec of the state
 const selectCart = state => state.cart;
 
-// output selector
+// output selectors
 export const selectCartItems  = createSelector([selectCart], (cart) => cart.cartItems);
+
+export const selectCartHidden = createSelector([selectCart], cart => cart.hidden);
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems => cartItems.reduce(
     (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
   )
-)
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0
+  )
+);
